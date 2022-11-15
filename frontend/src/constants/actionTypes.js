8b25627d27cba6f34131ad0a89be8939a8f5bab5
Kerry Meng-Lin <kerry@usc-securewireless-student-new50.usc.edu>
2022-11-14 21:14:51 -0800

    added general structure for actions such as creating posts,deleting,etc

---
+++ frontend/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+// export const CREATE = 'CREATE';
+// export const DELETE = 'DELETE';
+// export const FETCH_ALL = 'FETCH_ALL';
+// export const FETCH_BY_SEARCH = 'FETCH_BY_SEARCH';
+// export const RSVP = 'RSVP';
